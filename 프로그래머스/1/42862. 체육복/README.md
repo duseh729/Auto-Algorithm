48c3ece65c6c9454448cb939ecece740c8233dad
Doyeon <50562797+duseh729@users.noreply.github.com>
2024-01-18 09:51:06 +0900

    [level 1] Title: 체육복, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=javascript#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.10 ms
+메모리: 33.4 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 9:38:4
+2024년 1월 4일 9:51:4
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,6 +1,11 @@
 function solution(n, lost, reserve) {
     var answer = n-lost.length;
+    
+    // lost[4,2] reserve[3,5] 같은 케이스를 위한 정렬
     lost.sort((a,b)=>a-b)
+    reserve.sort((a,b)=>a-b)
+    
+    // 여벌은 가져왔는데 도난 당한 경우 처리
     for (i of lost){
         if (reserve.includes(i)){
             reserve = reserve.filter(element => element!==i)
@@ -10,6 +15,7 @@ function solution(n, lost, reserve) {
         }
     }
     
+    // 잃어버렸는데 여벌 빌릴 수 있는지, 아랫번호 학생 먼저 빌려야 최적의 해 구할 수 있음.
     for (i of lost){
         if (reserve.includes(i-1)){
             reserve = reserve.filter(element=> element!==i-1)
