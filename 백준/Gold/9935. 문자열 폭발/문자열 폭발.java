8c4bc10f1139011bedeee26ac811fcd318e51388
Doyeon <50562797+duseh729@users.noreply.github.com>
2024-07-04 17:45:19 +0900

    [Gold IV] Title: 문자열 폭발, Time: 480 ms, Memory: 62024 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,52 +1,38 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {	
+public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+   public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       String str = br.readLine();
+       String bomb = br.readLine();
+       int bombSize = bomb.length();
 
-		String str = br.readLine();
-		String bomb = br.readLine();
-		int str_len = str.length();
-		int bomb_len = bomb.length();
+       Stack<Character> stack = new Stack<>();
 
-		Stack<Character> stack = new Stack<>();
+       for(int i=0; i<str.length(); i++ ){
+           stack.push(str.charAt(i));
+           if(stack.size() >= bombSize) {
+               boolean flag = true;
 
-		for(int i=0; i<str_len; i++) {
-			int count = 0;
-			stack.push(str.charAt(i));
+               for(int j=0; j<bombSize; j++) {
+                   if(stack.get(stack.size() - bombSize + j) != bomb.charAt(j)) {
+                       flag = false;
+                       break;
+                   }
+               }
 
-			// 스택의 크기가 폭탄 문자열의 길이와 같아지면 탐색 시작
-			if(stack.size() >= bomb_len) {
-
-				for(int j=0; j<bomb_len; j++) {
-
-					if(stack.get(stack.size() - bomb_len + j) == bomb.charAt(j)) {
-						count++;
-					}	
-
-					if(count == bomb_len) {
-						for(int k=0; k<bomb_len; k++) {
-							stack.pop();
-						}
-					}
-
-				}
-			}
-		}
-
-		if(stack.isEmpty()) {
-			System.out.println("FRULA");
-		}
-		else {
-			for(char ch : stack) {
-				sb.append(ch);
-			}
-		}
-
-		System.out.println(sb);
-		
-	} // End of main
-} // End of class
+               if(flag) {
+                   for(int j=0; j<bombSize; j++) stack.pop();
+               }
+           }
+       }
+       if(stack.isEmpty()) System.out.println("FRULA");
+       else {
+           StringBuilder sb = new StringBuilder();
+           for(Character c : stack) sb.append(c);
+           System.out.println(sb);
+       }
+   }
+}
