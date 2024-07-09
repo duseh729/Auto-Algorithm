57433256acff906be0f713336869009ca855606b
Doyeon <50562797+duseh729@users.noreply.github.com>
2024-07-09 18:14:36 +0900

    [Silver I] Title: IOIOI, Time: 284 ms, Memory: 20180 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,39 +1,37 @@
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        String s = br.readLine();
+        String S = br.readLine();
 
-        StringBuilder target = new StringBuilder();
+        int cnt = 0;
+        int answer = 0;
 
-        target.append('I');
-        for(int i=0; i<n; i++){
-            target.append("OI");
-        }
-//        System.out.println(target.toString());
-        int answer=0;
-        for(int i=0; i<s.length()-target.length()+1; i++){
-            boolean trigger = true;
-            for(int j=0; j<target.length(); j++){
-                if(s.charAt(i+j) != target.charAt(j)){
-                    trigger = false;
-                    break;
+        for(int i = 1; i < M-1; i++){
+            if(S.charAt(i-1) == 'I' &&S.charAt(i) == 'O' && S.charAt(i+1) == 'I'){
+                cnt++;
+                if(cnt == N){
+                    cnt--;
+                    answer++;
                 }
+                i++;
             }
-            if(trigger){
-                answer++;
+            else{
+                cnt = 0;
             }
         }
-        bw.write(answer+"");
-
+        bw.write(String.valueOf(answer));
+        bw.flush();
         bw.close();
     }
 }
