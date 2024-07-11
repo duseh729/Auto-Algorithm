a54322fb6f1cb5552048c84f7be4dda086eed48e
Doyeon <50562797+duseh729@users.noreply.github.com>
2024-07-11 18:31:32 +0900

    [Gold IV] Title: 오큰수, Time: 1036 ms, Memory: 168444 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,68 +1,38 @@
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
-import java.util.Stack;
 
-class Num{
-    int index;
-    int value;
-
-    Num(int index, int value){
-        this.index = index;
-        this.value = value;
-    }
-}
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        String[] temp = br.readLine().split(" ");
 
-        String[] nums = br.readLine().split(" ");
-
-        int[] answer = new int[nums.length];
+        int[] answer = new int[temp.length];
         Arrays.fill(answer, -1);
-        // 4            4
-        // 3 5 2 7      9 5 4 8
-        // 5 7 7 -1     -1 8 8 -1
-        Stack<Num> numsStack = new Stack<>();
-        for(int i = 0; i < nums.length; i++){
-//            for(Num k: numsStack){
-//                System.out.print("i: " + i + " k: " + k.value);
-//            }
-//            System.out.println();
-            int num = Integer.parseInt(nums[i]);
-            if(numsStack.isEmpty()){
-                numsStack.push(new Num(i, num));
-                continue;
-            }
-            while (true){
-//                System.out.println(i + " while문 진입");
-                if(numsStack.isEmpty()){
-                    numsStack.push(new Num(i, num));
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=0; i<temp.length; i++){
+            while (true) {
+                if(stack.isEmpty()){
+                    stack.push(i);
                     break;
                 }
-                if(numsStack.peek().value<num){
-                    answer[numsStack.peek().index]=num;
-                    numsStack.pop();
-                }else if (numsStack.peek().value>=num){
-                    numsStack.push(new Num(i, num));
+                int current = Integer.parseInt(temp[stack.peek()]);
+                if(current<Integer.parseInt(temp[i])){
+                    answer[stack.pop()] = Integer.parseInt(temp[i]);
+                }else{
+                    stack.push(i);
                     break;
                 }
             }
         }
-
-//        for(Num k: numsStack){
-//            System.out.print(" k: " + k.value);
-//        }
-//        System.out.println();
-//        System.out.println(Arrays.toString(answer));
-
-        for (int i: answer){
+        for(int i: answer){
             bw.write(i+" ");
         }
 
-        bw.flush();
         bw.close();
     }
 }
