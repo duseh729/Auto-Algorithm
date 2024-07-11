69230760c087317bfc80cc75cd49b0ae295b3bbc
Doyeon <50562797+duseh729@users.noreply.github.com>
2024-07-11 18:03:52 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 644 ms, Memory: 66772 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -1,26 +1,27 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] temp = br.readLine().split(" ");
+        String[] nums = br.readLine().split(" ");
+        int n = Integer.parseInt(nums[0]);
+        int m = Integer.parseInt(nums[1]);
 
-        HashMap<String, String> password = new HashMap<>();
-        for(int i=0; i<Integer.parseInt(temp[0]); i++){
-            String[] passwordInput = br.readLine().split(" ");
+        HashMap<String, String> map = new HashMap<>();
+        for(int i=0; i<n; i++){
+            String[] temp = br.readLine().split(" ");
 
-            password.put(passwordInput[0], passwordInput[1]);
+            map.put(temp[0], temp[1]);
         }
 
-        for(int i=0; i<Integer.parseInt(temp[1]); i++){
-            String passwordInput = br.readLine();
+        for(int i=0; i<m; i++){
+            String temp = br.readLine();
 
-            bw.write(password.get(passwordInput)+"\n");
+            bw.write(map.get(temp)+"\n");
         }
-
         bw.close();
     }
 }
