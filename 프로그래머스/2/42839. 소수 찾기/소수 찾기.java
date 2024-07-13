723435146ab4c539d4af3dfb7b31339717238389
Doyeon <50562797+duseh729@users.noreply.github.com>
2024-07-13 22:45:39 +0900

    [level 2] Title: 소수 찾기, Time: 69.77 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,51 +1,42 @@
 import java.util.*;
+
 class Solution {
-    static int answer = 0;
-    Deque<Integer> sosu = new ArrayDeque<>();
-    // isPrime(Integer.parseInt(sb.toString()))
+    Set<Integer> nums = new HashSet<>();
     public int solution(String numbers) {
+        int answer = 0;
         boolean[] visited = new boolean[numbers.length()];
-        backtrack(numbers, visited, new StringBuilder());
-        // System.out.println(sosu);
-        for(int i: sosu){
-            if(isPrime(i)){
-                answer++;
-            }
+        backtrack(new StringBuilder(), numbers, visited);
+        for(int i: nums){
+            if(isPrime(i)) answer++;
         }
         return answer;
     }
-    
-    private void backtrack(String numbers, boolean[] visited, StringBuilder sb){
-        // System.out.println(sb);
-        // basecase
-        if(sb.length()>0 && !sosu.contains(Integer.parseInt(sb.toString()))){
-            // answer++;
-            // return;
-            sosu.push(Integer.parseInt(sb.toString()));
-        }
+    private void backtrack(StringBuilder sb, String numbers, boolean[] visited){
+        if(sb.length()>0) nums.add(Integer.parseInt(sb.toString()));
         
-        // recursive call
         for(int i=0; i<numbers.length(); i++){
             if(visited[i]) continue;
+            
+            visited[i] = true;
             sb.append(numbers.charAt(i));
             
-            visited[i]=true;
-            backtrack(numbers, visited, sb);
+            backtrack(sb, numbers, visited);
             
             sb.deleteCharAt(sb.length()-1);
-            visited[i]=false;
+            visited[i] = false;
         }
+        
     }
     
-    public boolean isPrime(int n) {
-        if(n==0 || n==1){
+    private boolean isPrime(int n){
+        if(n==0||n==1){
             return false;
         }
-        for (int i = 2; i<=(int)Math.sqrt(n); i++) {
-          if (n % i == 0) {
-              return false;
-          }
+        for(int i=2; i<=(int)n/2;i++){
+            if(n%i==0){
+                return false;
+            }
         }
-	return true;
-}
+        return true;
+    }
 }
