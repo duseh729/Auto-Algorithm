2dcc309ed795921a5197b4b04e9c69a4c1e065c3
Doyeon <50562797+duseh729@users.noreply.github.com>
2024-08-29 14:39:02 +0900

    [level 3] Title: 합승 택시 요금, Time: 48.27 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -3,21 +3,21 @@ class Solution {
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int[][] costs = new int[n+1][n+1];
         for(int[] fare: fares){
-            costs[fare[0]][fare[1]]=fare[2];
-            costs[fare[1]][fare[0]]=fare[2];
+            costs[fare[0]][fare[1]] = fare[2];
+            costs[fare[1]][fare[0]] = fare[2];
         }
+        
         int[][] distArray = new int[3][n+1];
         for(int[] d: distArray){
             Arrays.fill(d, Integer.MAX_VALUE);
         }
         
-        Queue<int[]> pq = new PriorityQueue<>((e1, e2)->e1[1]-e2[1]);
+        Queue<int[]> pq = new PriorityQueue<>((e1, e2)->e1[0]-e2[0]);
         int[] start = {s, a, b};
-        
         for(int i=0; i<3; i++){
-            pq.offer(new int[]{start[i], 0});
             int[] d = distArray[i];
             d[start[i]] = 0;
+            pq.offer(new int[]{start[i], 0});
             
             while(!pq.isEmpty()){
                 int[] cur = pq.poll();
@@ -26,9 +26,9 @@ class Solution {
                 
                 for(int j=1; j<=n; j++){
                     if(costs[node][j]==0) continue;
-                    if(d[j]>dist+costs[node][j]){
-                        d[j] = dist+costs[node][j];
-                        pq.offer(new int[]{j, dist+costs[node][j]});
+                    if(d[j]>costs[node][j]+dist){
+                        d[j]=costs[node][j]+dist;
+                        pq.offer(new int[]{j, costs[node][j]+dist});
                     }
                 }
             }
@@ -36,11 +36,11 @@ class Solution {
         
         int answer = Integer.MAX_VALUE;
         for(int i=1; i<=n; i++){
-            int sum = 0;
+            int sum=0;
             for(int[] d: distArray){
                 sum+=d[i];
             }
-            answer=Math.min(answer, sum);
+            answer=Math.min(sum, answer);
         }
         return answer;
     }
