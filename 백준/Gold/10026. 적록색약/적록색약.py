b44ba0b24b36dc4c2bbbf5b2a3ba42a8a3c4ad91
Doyeon <50562797+duseh729@users.noreply.github.com>
2025-04-06 09:17:39 +0900

    [Gold V] Title: 적록색약, Time: 152 ms, Memory: 39664 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -0,0 +1,70 @@
+import queue
+
+n = int(input())
+
+color_arr = []
+color_visited = []
+color_handicapped_visited = []
+
+for _ in range(n):
+    color_arr.append(list(input()))
+    color_visited.append([False]*n)
+    color_handicapped_visited.append([False]*n)
+
+# print(arr, color_visited)
+# print(color_handicapped_visited)
+    
+q = queue.Queue()
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+# 0번 index 색맹 아닌 사람, 1번 index 색맹인 사람
+answer = [0, 0]
+
+for x in range(n):
+    for y in range(n):
+        now_color = color_arr[x][y]
+
+        if color_visited[x][y]==False:
+            color_visited[x][y]=True
+
+            q.put([x, y])
+            while not q.empty():
+              temp = q.get()
+              current_x = temp[0]
+              current_y = temp[1]
+
+              for z in range(4):
+                  nx = dx[z] + current_x
+                  ny = dy[z] + current_y
+
+                  # print(nx, ny)
+
+                  if nx>=0 and nx<n and ny>=0 and ny<n and color_visited[nx][ny]==False and color_arr[nx][ny]==now_color:
+                      color_visited[nx][ny] = True
+                      q.put([nx, ny])
+
+            answer[0]+=1
+            
+        if color_handicapped_visited[x][y]==False:
+            color_handicapped_visited[x][y]=True
+
+            q.put([x, y])
+            while not q.empty():
+              temp = q.get()
+              current_x = temp[0]
+              current_y = temp[1]
+
+              for z in range(4):
+                  nx = dx[z] + current_x
+                  ny = dy[z] + current_y
+
+                  if nx>=0 and nx<n and ny>=0 and ny<n and color_handicapped_visited[nx][ny]==False:
+                      if now_color==color_arr[nx][ny] or (now_color=='R' and color_arr[nx][ny]=='G') or (now_color=='G' and color_arr[nx][ny]=='R'):
+                        color_handicapped_visited[nx][ny] = True
+                        q.put([nx, ny])
+
+            answer[1]+=1
+
+print(answer[0], answer[1])
