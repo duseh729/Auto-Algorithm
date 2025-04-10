f0bf743afb49d2392000bf06b2335e4e5c981ebe
Doyeon <50562797+duseh729@users.noreply.github.com>
2025-04-10 16:38:07 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 4580 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,60 @@
+input()
+
+number_arr = list(map(int, input().split(" ")))
+
+sign = list(map(int, input().split(" ")))
+
+# print(number)
+# print(sign)
+
+sign_arr = []
+
+sign_dict = {
+    0: "+",
+    1: "-",
+    2: "*",
+    3: "/",
+}
+
+for index, s in enumerate(sign):
+    # print(s, index)
+    s_clone = s
+    while s_clone>0:
+        sign_arr.append(sign_dict[index])
+        s_clone-=1
+
+# print(sign_arr)
+
+max_answer = -1000000000
+min_answer = 1000000000
+
+visited = [False] * len(sign_arr)
+
+def dfs(sign_arr, visited, value, count, index):
+    global max_answer, min_answer
+
+    if count==len(sign_arr):
+        max_answer = int(max(max_answer, value))
+        min_answer = int(min(min_answer, value))
+        return
+    
+    for i in range(len(sign_arr)):
+        if not visited[i]:
+            visited[i] = True
+            if sign_arr[i]=='+':
+                dfs(sign_arr, visited, value + number_arr[index], count+1, index + 1)
+            if sign_arr[i]=='-':
+                dfs(sign_arr, visited, value - number_arr[index], count+1, index + 1)
+            if sign_arr[i]=='*':
+                dfs(sign_arr, visited, value * number_arr[index], count+1, index + 1)
+            if sign_arr[i]=='/':
+                if value<0:
+                    dfs(sign_arr, visited, -(-value // number_arr[index]), count+1, index + 1)
+                else:
+                    dfs(sign_arr, visited, value // number_arr[index], count+1, index + 1)
+            visited[i] = False
+
+dfs(sign_arr, visited, number_arr[0], 0, 1)
+
+print(max_answer)
+print(min_answer)
